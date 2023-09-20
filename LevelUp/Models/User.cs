9c44b7fdbcb1489b359dccb625829cf4a6f84335
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-09-20 10:11:37 -0600

    Added user signup

--- LevelUp/Models/User.cs
+++ LevelUp/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks.Sources;
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks.Sources;
 
 namespace LevelUp.Models
 {
@@ -7,16 +9,32 @@ namespace LevelUp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
-        public string  Password { private get; set; }
+        public string  Password { get; set; }
         public List<DailyTask> DailyTasks { get; set; } = new List<DailyTask>();
-        public int Level { get; set; }
-        public int Xp { get; set; }
-        public int Hygiene { get; set; }
-        public int Wellness { get; set; }
-        public int Mindfullness { get; set; }
-        public int Productivity { get; set; }
-        public int HabitBuilding { get; set; }
+        public int Level { get; set; } = 1;
+        public int Xp { get; set; } = 0;
+        public int Hygiene { get; set; } = 0;
+        public int Wellness { get; set; } = 0;
+        public int Mindfullness { get; set; } = 0;
+        public int Productivity { get; set; } = 0;
+        public int HabitBuilding { get; set; } = 0;
 
+        public string Encrpyt(string password)
+        {
+            HashAlgorithm sha = SHA256.Create();
+            byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
+            byte[] passwordDigested = sha.ComputeHash(passwordBytes);
+            StringBuilder passwordBuilder = new StringBuilder();
+            foreach (byte b in passwordDigested)
+            {
+                passwordBuilder.Append(b.ToString("x2"));
+            }
+            return passwordBuilder.ToString();
+        }
 
+        public string EncryptPassword()
+        {
+            return Encrpyt(Password);
+        }
     }
 }
