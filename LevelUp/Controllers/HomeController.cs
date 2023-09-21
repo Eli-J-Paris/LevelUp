8dab2b0c3bf9d192a8c9d03d43598a67bde238c6
joe10111 <centeno.joe@gmail.com>
2023-09-21 10:16:18 -0700

    Removed Chart Data

--- LevelUp/Controllers/HomeController.cs
+++ LevelUp/Controllers/HomeController.cs
@@ -15,20 +15,6 @@ namespace LevelUp.Controllers
 
         public IActionResult Index()
         {
-            List<ChartData> chartData = new List<ChartData>
-            {
-                new ChartData{ x=2000, y1= 0.03, y2= 0.48},
-                new ChartData{ x=2001, y1= 0.05, y2= 0.53 },
-                new ChartData{ x=2002, y1= 0.06, y2= 0.57 },
-                new ChartData{ x=2003, y1= 0.09, y2= 0.61 },
-                new ChartData{ x=2004, y1= 0.14, y2= 0.63 },
-                new ChartData{ x=2005, y1= 0.20, y2= 0.64 },
-                new ChartData{ x=2006, y1= 0.29, y2= 0.66 },
-                new ChartData{ x=2007, y1= 0.46, y2= 0.76 },
-                new ChartData{ x=2008, y1= 0.64, y2= 0.77 },
-                new ChartData{ x=2009,  y1= 0.75, y2= 0.55 }
-            };
-            ViewBag.dataSource = chartData;
             return View();
         }
 
@@ -42,12 +28,5 @@ namespace LevelUp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
-
-    public class ChartData
-    {
-        public double x;
-        public double y1;
-        public double y2;
     }
 }
