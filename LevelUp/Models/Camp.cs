761b1bc65bdc896dffca7b83d2102dac1b6bc4ac
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-09-26 15:16:43 -0600

    commit

--- LevelUp/Models/Camp.cs
+++ LevelUp/Models/Camp.cs
@@ -5,6 +5,7 @@
         public int Id { get; set;}
         public string Title { get; set; }
         public string Description { get; set; }
+
         //public User Owner { get; set; }
         public List<User> Members { get; set; } = new List<User>();
         public List<Message> MessageBoard { get; set; } = new List<Message>();
