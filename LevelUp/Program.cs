80f10605c7b4dd5a4908333ea6538bd904a9c9f8
joe10111 <centeno.joe@gmail.com>
2023-10-18 14:32:16 -0700

    Added Try Catch Blocks for Log In and Program start up

--- LevelUp/Program.cs
+++ LevelUp/Program.cs
@@ -6,43 +6,56 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console().WriteTo.File("logs/mvcmovie.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-
-builder.Services.AddDbContext<LevelUpContext>(
-    options =>
-        options
-            .UseNpgsql(builder.Configuration["LEVELUP_DBCONNECTIONSTRING"])
-            .UseSnakeCaseNamingConvention()
-);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try 
 {
-    app.UseExceptionHandler("/Error/Error");
-    app.UseHsts();
+    Log.Information("Starting Level Up application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddDbContext<LevelUpContext>(
+        options =>
+            options
+                .UseNpgsql(builder.Configuration["LEVELUP_DBCONNECTIONSTRING"])
+                .UseSnakeCaseNamingConvention()
+    );
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error/Error");
+        app.UseHsts();
+    }
+
+    // Handles status codes like 404
+    app.UseStatusCodePagesWithReExecute("/Error/Error", "?statusCode={0}");
+
+
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-// Handles status codes like 404
-app.UseStatusCodePagesWithReExecute("/Error/Error", "?statusCode={0}");
 
-
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
