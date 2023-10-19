0c36ef25f3cfc5f70057930e80e778462ab7673c
jcepriano <jamescepriano@gmail.com>
2023-10-19 14:43:13 -0700

    Reactive List is working

---
+++ LevelUp/Models/StrongPasswordAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class StrongPasswordAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value is string password)
+        {
+            // Add your password criteria here.
+            if (password.Length < 8)
+            {
+                ErrorMessage = "Password must be at least 8 characters long.";
+                return false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                ErrorMessage = "Password must contain at least one uppercase letter.";
+                return false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                ErrorMessage = "Password must contain at least one digit.";
+                return false;
+            }
+
+            // Add more criteria as needed.
+        }
+
+        return true;
+    }
+}
